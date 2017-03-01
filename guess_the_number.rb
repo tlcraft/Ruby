0cbb40f657e9bd0d1fce9cb41046defe768100f4
tlcraft <tlcraft@users.noreply.github.com>
2017-02-28 22:56:44 -0700

    Updated the game a little
    Added a method to check how hot or cold a guess is to the secret number.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -7,16 +7,31 @@ def guess(secret)
 		puts "You got it right!"
 		isFinished = true
 	else
-		puts "Wrong answer, try agian."
+		hot_or_cold(guess, secret)
 	end
 
 	return isFinished
 end
 
-SECRET_NUMBER = rand(0..10)+1
+def hot_or_cold (guess, answer)
+	hot_or_cold = (answer - guess).abs
+
+	if hot_or_cold <= 5
+		puts "Very hot! Almost got it!"
+	elsif hot_or_cold < 15
+		puts "You're warm, guess again."
+	elsif hot_or_cold < 25
+		puts "A little warmer, keep trying."
+	else
+		puts "Cold, try again."
+	end
+end
+
+MAX_VALUE = 100
+SECRET_NUMBER = rand(0..MAX_VALUE)+1
 
 count = 1
-puts "Guess the number! (1 - 10) How many guesses will it take you?"
+puts "Guess the number! (1 - #{MAX_VALUE}) How many guesses will it take you?"
 
 until guess(SECRET_NUMBER) do
 	count += 1
